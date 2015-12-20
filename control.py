3cc1f4a3e30b10557c62d9fb9d8b849c62b07088
Grzegorz Sygieda <grzegorz.sygieda@gmail.com>
2015-12-20 19:41:48 +0100

    Initial drop.

---
+++ control.py
@@ -0,0 +1,30 @@
+import serial
+from time import sleep
+
+class control:
+
+    def __init__(self, port):
+        self.port = port
+        self.out = serial.Serial(port, 115200, timeout=0)
+
+    def __del__(self):
+        self.out.close()
+
+    def left(self):
+        self.out.write('0p0100000000')
+
+    def rigth(self):
+        self.out.write('0p0200000000')
+
+    def forward(self, power):
+        self.out.write('0p0400ff0000')
+
+    def backward(self, power):
+        self.out.write('0p0800ff0000')
+
+
+if __name__ == "__main__":
+    port = "/dev/rfcomm0"
+    ctrl = control(port)
+    ctrl.forward(100)
+    ctrl.left()
